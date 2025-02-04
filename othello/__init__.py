c41fe5d905d5a53f3a019f430dce9d798b2d7ba5
Luk <lucasmarques@proton.me>
2025-02-04 16:45:57 +0100

    pushing updated setuptools

---
+++ othello/__init__.py
@@ -0,0 +1,6 @@
+"""Main module for myproject"""
+
+import othello.othello
+
+__version__ = "0.1.0"
+
---
+++ othello/othello.py
@@ -0,0 +1,4 @@
+"""The main module of the project"""
+def main() -> None:
+    """Entrypoint of the project"""
+    print("Hello, World!")
