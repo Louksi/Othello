8631f651b8945f237e2b4936fefaaff17c64c087
Gabriel Tardiou <gabrielle.tardiou@etu.u-bordeaux.fr>
2025-02-12 21:12:33 +0100

    BlitzTimer updated

--- othello/othello/blitzTimer.py
+++ othello/othello/blitzTimer.py
@@ -14,49 +14,49 @@ from time import time
 
 # def displayTime()
 
+
 class BlitzTimer:
 
     def __init__(self, timeLimit) -> None:
-      self.startTime = None
-      self.totalTime = timeLimit * 60 # converts the time limit from minutes to seconds
-      self.remainingTime = {
-        'black': self.totalTime,
-        'white': self.totalTime
-      }
-      self.currentPlayer = None
-
-    def startTimer(self, player) -> None:
-      self.startTime = time()
-      self.currentPlayer = player
-
-    def pauseTimer(self) -> None:
-      if self.startTime and self.currentPlayer:
-        self.remainingTime[self.currentPlayer] = max(0, self.remainingTime[self.currentPlayer] - (time() - self.startTime))
         self.startTime = None
+        self.totalTime = timeLimit * 60  # converts the time limit from minutes to seconds
+        self.remainingTime = {
+            'black': self.totalTime,
+            'white': self.totalTime
+        }
         self.currentPlayer = None
 
+    def startTimer(self, player) -> None:
+        self.startTime = time()
+        self.currentPlayer = player
+
+    def pauseTimer(self) -> None:
+        if self.startTime and self.currentPlayer:
+            self.remainingTime[self.currentPlayer] = max(
+                0, self.remainingTime[self.currentPlayer] - (time() - self.startTime))
+            self.startTime = None
+            self.currentPlayer = None
+
     def changePlayer(self, player) -> None:
-      self.pauseTimer()
-      self.startTimer(player)
+        self.pauseTimer()
+        self.startTimer(player)
 
     def getRemainingTime(self, player) -> float:
-      if self.startTime and player == self.currentPlayer:
-        return max(0, self.remainingTime[self.currentPlayer] - (time() - self.startTime))
-      return self.remainingTime[player]
+        if self.startTime and player == self.currentPlayer:
+            return max(0, self.remainingTime[self.currentPlayer] - (time() - self.startTime))
+        return self.remainingTime[player]
 
     def isTimeUp(self, player) -> bool:
-      return self.getRemainingTime(player) <= 0
+        return self.getRemainingTime(player) <= 0
 
     def displayTime(self) -> None:
-      blackTime = int(self.getRemainingTime('black'))
-      whiteTime = int(self.getRemainingTime('white'))
+        blackTime = int(self.getRemainingTime('black'))
+        whiteTime = int(self.getRemainingTime('white'))
 
-      BMins = blackTime // 60
-      WMins = whiteTime // 60
+        BMins = blackTime // 60
+        WMins = whiteTime // 60
 
-      BSecs = blackTime % 60
-      WSecs = whiteTime % 60
+        BSecs = blackTime % 60
+        WSecs = whiteTime % 60
 
-      return f"Black Time: {BMins:02d}:{BSecs:02d}\nWhite Time: {WMins:02d}:{WSecs:02d}\n"
-
-
+        return f"Black Time: {BMins:02d}:{BSecs:02d}\nWhite Time: {WMins:02d}:{WSecs:02d}\n"
