f92799e2999804c1df407a27ea8681cef072a228
Luk <lucasmarques@proton.me>
2025-02-12 22:37:18 +0100

    removed useless class, updated main accordingly

--- othello/othello/__main__.py
+++ othello/othello/__main__.py
@@ -1,18 +1,2 @@
-import othello.othello
-from othello.othello_bitboard import OthelloBitboard, BoardSize, Color
-
-
-def main():
-    othello.othello.main()
-
-
 if __name__ == "__main__":
-    b = OthelloBitboard(BoardSize.EIGHT_BY_EIGHT)
-    print(b.line_cap_move(Color.WHITE))
-    print()
-    cap = b.line_cap(4, 2, Color.WHITE)
-    print(cap)
-    print()
-    b.white = b.white | cap
-    b.black.bits = b.black.bits & ~(cap.bits)
-    print(b)
+    print("Hello, World")
--- othello/othello/illegal_size_exception.py
+++
@@ -1,3 +0,0 @@
-class IllegalSize(Exception):
-    def __init__(self, size: int):
-        self.message = f"Can't make board of size {size}"
