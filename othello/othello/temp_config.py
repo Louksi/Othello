2b663532e6577df9a22a60a60d3efc880bbb7ee2
Gabriel Tardiou <gabrielle.tardiou@etu.u-bordeaux.fr>
2025-02-18 16:03:33 +0100

    tmp config

---
+++ othello/othello/temp_config.py
@@ -0,0 +1,31 @@
+# CONFIG FILE
+# mode = normal/blitz/contest/ai
+# filename = save.feur/None
+# size = 6/8/10/12
+# debug = false/true
+# blitz_time = 30/X (en minutes)
+# ai_color = X/O/A
+# ai_mode = minimax/alphabeta
+# ai_shallow = true/false
+# ai_depth = 3/X (sachant que root_depth = 0)
+# ai_heuristic = default/autre
+# ai_time = 5/X (en secondes)
+
+
+def save_config(config):
+  pass
+# create file
+# enter all args (see first 12 lines)
+# save
+
+def load_config(filename):
+  pass
+# get configfile from file
+# load all arguments in a dict
+
+
+def main():
+  pass
+#parse
+#save config
+#load config
