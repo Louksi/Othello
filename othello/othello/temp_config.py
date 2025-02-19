2476e580ab523342b6f1050593584b5de082f879
Gabriel Tardiou <gabrielle.tardiou@etu.u-bordeaux.fr>
2025-02-19 09:56:41 +0100

    tmp config almost done Tests to do

--- othello/othello/temp_config.py
+++ othello/othello/temp_config.py
@@ -12,20 +12,63 @@
 # ai_time = 5/X (en secondes)
 
 
-def save_config(config):
-  pass
-# create file
-# enter all args (see first 12 lines)
-# save
-
-def load_config(filename):
-  pass
-# get configfile from file
-# load all arguments in a dict
+from othello.parser import parse_args, default_config
 
 
-def main():
-  pass
-#parse
-#save config
-#load config
+def save_config(config, filename_prefix="default"):
+    """
+    Save configuration into a .othellorc file, .ini format
+    """
+    filename = f"{filename_prefix}.othellorc"
+    try:
+        with open(filename, "w") as f:
+            for key, value in config.items():
+                f.write(f"{key}={value}\n")
+    except IOError as e:
+        print(f"Error while saving configuration: {e}")
+
+
+def load_config(filename_prefix="default") -> dict:
+    """
+    Loading a configuration from a .othellorc file
+
+    Returns:
+      dict: Dictionnary containing the configuration from the file
+    """
+    filename = f"{filename_prefix}.othellorc"
+    config = {}
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                if "=" in line:
+                    key, value = line.strip().split("=", 1)
+                    config[key] = value
+    except FileNotFoundError:
+        print("No config file found, will take default configuration.")
+    except Exception as e:
+        print(f"Error while loading the configuration: {e}")
+    return config
+
+
+# def main():
+
+#     mode, config = parse_args()
+
+#     current_config = default_config.copy()
+#     current_config.update(config)
+
+#     # Converts all bool values into str
+#     for key in current_config:
+#         if isinstance(current_config[key], bool):
+#             current_config[key] = str(current_config[key]).lower()
+
+#     filename_prefix = "config"
+
+#     save_config(current_config, filename_prefix)
+
+#     loaded_config = load_config(filename_prefix)
+#     print("Config loaded:", loaded_config)
+
+
+# if __name__ == "__main__":
+#     main()
