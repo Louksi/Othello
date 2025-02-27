66cb99fc76475a34baefe056c745bf9d4ff155fa
Rémy Heuret <remy.heuret@etu.u-bordeaux.fr>
2025-02-27 15:19:25 +0100

    alphabeta + coin parity

--- othello/othello/ai_features.py
+++ othello/othello/ai_features.py
@@ -1,6 +1,7 @@
 import sys
 import othello.parser as parser
 from othello.othello_board import BoardSize, OthelloBoard, Color, GameOverException
+from othello.bitboard import Bitboard
 
 
 def minimax(board: OthelloBoard, depth: int, max_player: Color, maximizing: bool) -> int:
@@ -23,32 +24,92 @@ def minimax(board: OthelloBoard, depth: int, max_player: Color, maximizing: bool
     :rtype: int
     """
 
-    try:
-        if depth == 0:
-            return corners_captured_heuristic(board, max_player)
+    if depth == 0 or board.is_game_over():
+        return corners_captured_heuristic(board, max_player)
 
-        eval = float("-inf") if maximizing else float("inf")
+    if maximizing:
+        eval = float("-inf")
         for move_x in range(board.size.value):
             for move_y in range(board.size.value):
                 if board.line_cap_move(board.current_player).get(move_x, move_y):
-                    print(board.export())
-                    try:
-                        # Need a copy, problem for future us
-                        board.play(move_x, move_y)
-                    except GameOverException:
-                        return corners_captured_heuristic(board, max_player)
-
+                    # Need a copy, problem for future us
+                    board.play(move_x, move_y)
                     eval_score = minimax(
                         board, depth - 1, max_player, not maximizing)
-                    if maximizing:
-                        eval = max(eval, eval_score)
-                    else:
-                        eval = min(eval, eval_score)
+                    eval = max(eval, eval_score)
+                    board.pop()
+    else:
+        eval = float("inf")
+        for move_x in range(board.size.value):
+            for move_y in range(board.size.value):
+                if board.line_cap_move(board.current_player).get(move_x, move_y):
+                    # Need a copy, problem for future us
+                    board.play(move_x, move_y)
+                    eval_score = minimax(
+                        board, depth - 1, max_player, maximizing)
+                    eval = min(eval, eval_score)
+                    board.pop()
 
-        return eval
+    return eval
 
-    except GameOverException:
+
+def alphabeta(board: OthelloBoard, depth: int, alpha: int, beta: int, max_player: Color, maximizing: bool) -> int:
+    """
+    Implements the Alpha-Beta Pruning algorithm to evaluate the best possible move for a given board state.
+
+    This function recursively explores possible moves up to a given depth, evaluating each position
+    using the Corners Captured heuristic when the maximum depth is reached or the game is over.
+    It returns the heuristic value of the best move found from the current board state.
+
+    :param board: The current state of the Othello board.
+    :type board: OthelloBoard
+    :param depth: The maximum depth to explore in the game tree.
+    :type depth: int
+    :param alpha: The best possible score for the maximizing player.
+    :type alpha: int
+    :param beta: The best possible score for the minimizing player.
+    :type beta: int
+    :param max_player: The player for whom the best move is being calculated (BLACK or WHITE).
+    :type max_player: Color
+    :param maximizing: A Boolean indicating whether the current step is maximizing or minimizing the score.
+    :type maximizing: bool
+    :return: The heuristic value of the best move found from the current board state.
+    :rtype: int
+    """
+
+    if depth == 0 or board.is_game_over():
         return corners_captured_heuristic(board, max_player)
+
+    if maximizing:
+        eval = float("-inf")
+        for move_x in range(board.size.value):
+            for move_y in range(board.size.value):
+                if board.line_cap_move(board.current_player).get(move_x, move_y):
+                    # Need a copy, problem for future us
+                    board.play(move_x, move_y)
+                    eval_score = alphabeta(
+                        board, depth - 1, alpha, beta, max_player, not maximizing)
+                    eval = max(eval, eval_score)
+                    board.pop()
+                    alpha = max(alpha, eval)
+                    if beta <= alpha:
+                        break
+    else:
+        eval = float("inf")
+        for move_x in range(board.size.value):
+            for move_y in range(board.size.value):
+                if board.line_cap_move(board.current_player).get(move_x, move_y):
+                    # Need a copy, problem for future us
+                    board.play(move_x, move_y)
+                    eval_score = alphabeta(
+                        board, depth - 1, alpha, beta, max_player, maximizing)
+                    eval = min(eval, eval_score)
+                    board.pop()
+                    beta = min(beta, eval)
+                    if beta <= alpha:
+                        break
+
+    return eval
 
 
 def corners_captured_heuristic(board: OthelloBoard, max_player: Color) -> int:
@@ -85,3 +146,11 @@ def corners_captured_heuristic(board: OthelloBoard, max_player: Color) -> int:
     if (max_corners + min_corners != 0):
         return int(100 * (max_corners - min_corners) / (max_corners + min_corners))
     return 0
+
+
+def coin_parity_heuristic(board: OthelloBoard, max_player: Color) -> int:
+
+    if max_player == Color.BLACK:
+        return int(100 * (board.black.popcount() - board.white.popcount())/(board.black.popcount() + board.white.popcount()))
+    if max_player == Color.WHITE:
+        return int(100 * (board.white.popcount() - board.black.popcount())/(board.white.popcount() + board.black.popcount()))
