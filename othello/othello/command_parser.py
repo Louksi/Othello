3839b95abd939be64dda07ea08eab190e6b8e2f8
Matis Duval <matis.duval@u-bordeaux.fr>
2025-03-03 19:43:18 +0100

    work in progress parser implementation

--- othello/othello/command_parser.py
+++ othello/othello/command_parser.py
@@ -4,6 +4,7 @@ A class to help parsing cli user input
 from dataclasses import dataclass
 from enum import Enum, auto
 import re
+import argparse
 from typing import Tuple, Union
 
 
@@ -27,6 +28,7 @@ class CommandKind(Enum):
     FORFEIT = auto()
     RESTART = auto()
     PLAY_MOVE = auto()
+    QUIT = auto()
 
 
 @dataclass
@@ -46,7 +48,8 @@ CommandType = Union[  # This type is used to kind of mimic some sort of algebrai
         CommandKind.SAVE_AND_QUIT,
         CommandKind.SAVE_HISTORY,
         CommandKind.FORFEIT,
-        CommandKind.RESTART]]
+        CommandKind.RESTART,
+        CommandKind.QUIT]]
 ]
 
 
@@ -56,14 +59,91 @@ class CommandParser:
     """
 
     def __init__(self, board_size: int):
-        str_board_max_column = chr(ord('a') + board_size)
+        # Fix to include the last column
+        str_board_max_column = chr(ord('a') + board_size - 1)
         str_board_max_line = board_size
         column_rx = rf"[a-{str_board_max_column}]"
         line_rx = rf"[1-{str_board_max_line}]"
-        command_regex_str = rf"(\?|{column_rx}{line_rx}|restart|r|sh|s|ff)"
+        command_regex_str = rf"(\?|{column_rx}{line_rx}|restart|r|sh|s|ff|q)"
         move_regex_str = rf"(({column_rx})({line_rx}))"
         self.command_regex = re.compile(command_regex_str)
         self.move_regex = re.compile(move_regex_str)
+
+        # Set up argparse for help display
+        self.help_parser = argparse.ArgumentParser(
+            description='Othello Game Commands',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            add_help=False  # Don't add the default help
+        )
+
+        # Add command descriptions
+        self.help_parser.add_argument(
+            '?', action='store_true', help='Show this help message')
+        self.help_parser.add_argument(
+            'r', action='store_true', help='Show the game rules')
+        self.help_parser.add_argument(
+            's', action='store_true', help='Save game and quit')
+        self.help_parser.add_argument(
+            'sh', action='store_true', help='Save game history')
+        self.help_parser.add_argument(
+            'ff', action='store_true', help='Forfeit the current game')
+        self.help_parser.add_argument(
+            'restart', action='store_true', help='Restart the game')
+        self.help_parser.add_argument(
+            'q', action='store_true', help='Quit without saving')
+        self.help_parser.add_argument(
+            'move',
+            metavar=f'[a-{str_board_max_column}][1-{str_board_max_line}]',
+            nargs='?',
+            help=f'Play a move (e.g., a1, {str_board_max_column}{str_board_max_line})'
+        )
+
+    def print_help(self):
+        """
+        Display help information using argparse.
+        """
+        print("\nOthello Game Help")
+        print("=================")
+        self.help_parser.print_help()
+        print("\nExample commands:")
+        print("  a1     - Play at position a1")
+        print("  ?      - Show this help message")
+        print("  r      - Show the game rules")
+        print("  q      - Quit without saving")
+        print("  s      - Save and quit")
+        print("  sh     - Save game history")
+        print("  ff     - Forfeit the current game")
+        print("  restart - Restart the game")
+        print("\nCoordinate format: [column][row] (e.g., a1, b2)")
+
+    def print_rules(self):
+        """
+        Display the rules of Othello/Reversi
+        """
+        print("\nOthello/Reversi Rules")
+        print("====================")
+        print("Objective:")
+        print("  The goal is to have the majority of your color discs on the board when the game ends.")
+        print("\nSetup:")
+        print("  - The game is played on an 8×8 board (though our implementation may vary)")
+        print("  - The game begins with four discs placed in the center in a 2×2 pattern,")
+        print("    with same-colored discs positioned diagonally.")
+        print("  - Black moves first")
+        print("\nGameplay:")
+        print("  1. A move consists of placing a disc of your color on an empty square")
+        print("  2. For a move to be valid, it must 'outflank' at least one of your opponent's discs")
+        print("  3. To outflank means to place a disc such that one or more of your opponent's discs")
+        print("     are bordered at each end by a disc of your color (in a straight line)")
+        print(
+            "  4. All of the opponent's discs that are outflanked are flipped to your color")
+        print("  5. If a player cannot make a valid move, their turn is skipped")
+        print("  6. The game ends when neither player can make a valid move")
+        print("\nWinning:")
+        print(
+            "  The player with the most discs of their color on the board at the end wins.")
+        print("  If both players have the same number of discs, the game is a draw.")
+        print("\nPress Enter to continue playing...")
+        input()
 
     def parse_str(self, command_str: str) -> CommandType:
         """
@@ -75,7 +155,8 @@ class CommandParser:
         :returns: A constructed command tuple
         :raises CommandParserException: if the string is invalid
         """
-        matches = self.command_regex.match(command_str)
+        matches = self.command_regex.fullmatch(command_str)
+
         if matches is None:
             raise CommandParserException(command_str)
         match_result = matches.group(1)
@@ -91,6 +172,8 @@ class CommandParser:
             return (CommandKind.FORFEIT,)
         elif match_result == "restart":
             return (CommandKind.RESTART,)
+        elif match_result == "q":
+            return (CommandKind.QUIT,)
         else:
             play_matches = self.move_regex.match(match_result)
             col_raw = play_matches.group(2)
