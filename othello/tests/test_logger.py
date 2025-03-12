2adc3b85caec75257c9cd62fb827541b16827dd9
Gabriel Tardiou <gabrielle.tardiou@etu.u-bordeaux.fr>
2025-03-12 16:22:29 +0100

    test logger

---
+++ othello/tests/test_logger.py
@@ -0,0 +1,32 @@
+import logging
+import pytest
+from unittest.mock import patch, MagicMock
+from othello.logger import logging_config, log_error_message
+
+
+# TEST INITIALIZATION
+
+# with debug mode
+
+# without debug mode
+
+
+
+# TEST DEBUG OUTPUT
+
+# in a file
+
+# in the console
+
+# log error with context
+
+# log error without context
+
+# log error writes exc_info
+
+
+
+# TEST ERRORS
+
+#
+
