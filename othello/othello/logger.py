5015d68889c76ea33cab18a79a889b719c46f0ce
Gabriel Tardiou <gabrielle.tardiou@etu.u-bordeaux.fr>
2025-03-13 12:28:30 +0100

    logger + tests (some commented for compatibility sake)

--- othello/othello/logger.py
+++ othello/othello/logger.py
@@ -1,15 +1,21 @@
+"""
+Logger used for debug mode.
+Uses logging to display debug messages and error messages.
+"""
 import logging
 
 
 def logging_config(debug: bool) -> None:
     """
     Set up the logger if the debug mode is enabled.
-    Write the log onto the console and in a file.
+    Write the log in a file.
+    Uncomment the line 'logging.StreamHandler() to have the log written onto the console.
 
     Argument:
       debug: a boolean used to set the logging level
     """
-
+    # if not isinstance(debug, bool):
+    #     raise TypeError()
     if debug:
         level = logging.DEBUG
         logging.basicConfig(level=level,
@@ -29,6 +35,11 @@ def log_error_message(error: str, context: str | None = None) -> None:
       error: a string of the error message to be logged
       context: a string describing the context of the error, can be None if no context is given
     """
+    # if not isinstance(error, str):
+    #     raise TypeError()
+    # if context is not None and not isinstance(context, str):
+    #     raise TypeError()
+
     logger = logging.getLogger("Othello")
     if context is not None:
         logger.error(f"Context: {context}")
